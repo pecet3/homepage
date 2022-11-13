04c0236a541c45a077373edd02c1bfb6f7594038
Jakub Pacewicz <pecet3107@gmail.coim>
2022-11-13 14:53:54 +0100

    improved js script for better readable

--- js/script.js
+++ js/script.js
@@ -1,61 +1,81 @@
-console.log("Witaj developerze ;D")
-let image = document.querySelector(".js-image");
-let buttonLeft = document.querySelector(".js-buttonLeft");
-let buttonRight = document.querySelector(".js-buttonRight");
+{
+    // variables needed for multiple functions
 
-buttonLeft.addEventListener('click', () => {
-    image.src = "https://i.ibb.co/njr61Jd/image1.jpg";
-})
+    const image = document.querySelector(".js-image");
+    const themeButton = document.querySelector(".js-themeButton");
 
-buttonRight.addEventListener('click', () => {
-    image.src = "https://i.ibb.co/pKkmXcs/image2.jpg";
-})
 
-let themeButton = document.querySelector(".js-header__themeButton");
-let body = document.querySelector(".js-body");
-let title = document.querySelector(".js-header__title");
-let main = document.querySelector(".js-main");
-let header = document.querySelector(".js-main__header");
-let header2 = document.querySelector(".js-main__header2");
+    // functions needed for change the main photo
+    const onImageButtonLeftClick = () => {
+        image.src = "https://i.ibb.co/njr61Jd/image1.jpg";
+    };
+    const onImageButtonRightClick = () => {
+        image.src = "https://i.ibb.co/pKkmXcs/image2.jpg";
+    };
 
-let mainPhoto = document.querySelector(".js-image");
-let footer = document.querySelector(".js-footer");
-let navigation = document.querySelector(".js-navigation");
-let caption = document.querySelector(".js-table__caption");
+    // functions needed for change a theme
+    const onThemeButtonClick = () => {
+        const body = document.querySelector(".js-body");
+        const title = document.querySelector(".js-header__title");
+        const main = document.querySelector(".js-main");
+        const header = document.querySelector(".js-main__header");
+        const header2 = document.querySelector(".js-main__header2");
+        const mainPhoto = document.querySelector(".js-image");
+        const footer = document.querySelector(".js-footer");
+        const navigation = document.querySelector(".js-navigation");
+        const caption = document.querySelector(".js-table__caption");
 
-themeButton.addEventListener('click', () => {
-    title.classList.toggle('header__title--dark');
-    body.classList.toggle('body--dark');
-    main.classList.toggle('main--dark');
-    header.classList.toggle('main__header--dark');
-    header2.classList.toggle('main__header--dark');
-    mainPhoto.classList.toggle('header__photo--dark');
-    footer.classList.toggle('footer--dark');
-    navigation.classList.toggle('navigation--dark');
-    caption.classList.toggle('main__header--dark');
-    // themeButton.innerText = body.classList.contains("dark")
-    // ? "🌙"
-    // : "🌞"
-    // I dunno why it doesn't work...
-    if (themeButton.innerText == "🌙") {
-        themeButton.innerText = "🌞";
-    }
-    else {
-        themeButton.innerText = "🌙";
-    }
-});
+        title.classList.toggle('header__title--dark');
+        body.classList.toggle('body--dark');
+        main.classList.toggle('main--dark');
+        header.classList.toggle('main__header--dark');
+        header2.classList.toggle('main__header--dark');
+        mainPhoto.classList.toggle('header__photo--dark');
+        footer.classList.toggle('footer--dark');
+        navigation.classList.toggle('navigation--dark');
+        caption.classList.toggle('main__header--dark');
+        themeButtonIconSwitch();
+    };
+    const themeButtonIconSwitch = () => {
+        if (themeButton.innerText == "🌙") {
+            themeButton.innerText = "🌞";
+        }
+        else {
+            themeButton.innerText = "🌙";
+        }
+    };
 
-let price1 = Number(document.querySelector(".js-price1").innerText);
-let price2 = Number(document.querySelector(".js-price2").innerText);
-let price3 = Number(document.querySelector(".js-price3").innerText);
-let price4 = Number(document.querySelector(".js-price4").innerText);
-let price5 = Number(document.querySelector(".js-price5").innerText);
-let price6 = Number(document.querySelector(".js-price6").innerText);
-let finalAmount = document.querySelector(".js-finalAmount");
+    // functions for table 
 
-function addPrices(price1, price2, price3, price4, price5, price6) {
-    let result = price1 + price2 + price3 + price4 + price5 + price6;
-    finalAmount.innerText = result;
-}
+    const addPrices = () => {
+        const price1 = Number(document.querySelector(".js-price1").innerText);
+        const price2 = Number(document.querySelector(".js-price2").innerText);
+        const price3 = Number(document.querySelector(".js-price3").innerText);
+        const price4 = Number(document.querySelector(".js-price4").innerText);
+        const price5 = Number(document.querySelector(".js-price5").innerText);
+        const price6 = Number(document.querySelector(".js-price6").innerText);
 
-addPrices(price1, price2, price3, price4, price5, price6);
+        return price1 + price2 + price3 + price4 + price5 + price6;
+    };
+
+
+    const typeFinalValueOfGear = () => {
+        const finalValueOfGear = document.querySelector(".js-finalValueOfGear");
+        console.log(addPrices());
+        finalValueOfGear.innerText = addPrices();
+    };
+
+    ////////////////////////////////////////////////////////////////
+    
+    const init = () => {
+        console.log("Witaj w konsoli :)");
+        themeButton.addEventListener('click', (onThemeButtonClick));
+        typeFinalValueOfGear();
+        const buttonLeft = document.querySelector(".js-buttonLeft");
+        const buttonRight = document.querySelector(".js-buttonRight");
+        buttonLeft.addEventListener('click', (onImageButtonLeftClick));
+        buttonRight.addEventListener('click', (onImageButtonRightClick));
+    };
+
+    init();
+};
