a95b9f01248b109a26da7a8ffbfc271aacc4eadf
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-17 00:39:48 +0100

    added new color, and breakpoints

--- src/theme.js
+++ src/theme.js
@@ -4,8 +4,13 @@ export const theme = {
         slateGray: "#6E7E91",
         iron: "#D1D5DA",
         mercury: "#E5E5E5",
+        athensGray: "#F0F2F5",
         white: "#FFFFFF",
         scienceBlue: "#0366D6",
+    },
+
+    breakpoints:{
+        mobile: 767,
     },
 
 };
