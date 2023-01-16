aba7e90f9487dd4e67adae9603a36b3a46cef5a0
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-16 16:07:42 +0100

    created the file

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+    colors:{
+        mineShaft: "#252525",
+        slateGray: "#6E7E91",
+        iron: "#D1D5DA",
+        mercury: "#E5E5E5",
+        white: "#FFFFFF",
+        scienceBlue: "#0366D6",
+    },
+
+};
