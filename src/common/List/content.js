750d5025a002aaafaf71ecd580e0bfded652d78d
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-17 23:30:27 +0100

    created file with content for list

---
+++ src/common/List/content.js
@@ -0,0 +1,26 @@
+export const skillset = [
+    "Semantic & accessible HTML",
+    "Basics of CSS 3",
+    "JavaScript ES6+",
+    "Git",
+    "CSS BEM convention",
+    "React.js",
+    "Scrum",
+    "CSS Flexbox",
+    "Redux (Toolkit)",
+    "Responsive Web Design",
+    "CSS Grid",
+    "Redux-Saga",
+    "Markdown",
+    "GitHub Pull Requests & Review",
+    "React Router",
+];
+
+export const nextToLearn = [
+    "TypeScript",
+    "Node.js",
+    "React Native",
+    "Express.js",
+    "React Context",
+    "SQL",
+];
