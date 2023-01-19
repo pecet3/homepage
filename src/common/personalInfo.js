88277156c9da12fa7d6d9f5b4ec323f0ebc367c9
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-19 19:23:26 +0100

    added github

--- src/common/personalInfo.js
+++ src/common/personalInfo.js
@@ -3,3 +3,5 @@ export const email = "jakub@pacewicze.pl";
 export const facebook = "https://www.facebook.com/jakub.pacewicz.9";
 
 export const discord = "https://discordapp.com/users/pecet#3630";
+
+export const github = "https://github.com/pecet3";
