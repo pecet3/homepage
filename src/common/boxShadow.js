4da319d4914817aeaa3ec7d842216a9fa87b5044
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-19 01:21:33 +0100

    renamed file, added new contst

--- src/common/boxShadow.js
+++
@@ -1,1 +0,0 @@
-export const boxShadow = "0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03)";
---
+++ src/common/propsCSS.js
@@ -0,0 +1,3 @@
+export const boxShadow = "0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03)";
+
+export const borderRadius = "4px";
