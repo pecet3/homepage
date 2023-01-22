bbdebcc4d48843d028d049e9023663d1bf41132f
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-22 22:56:47 +0100

    styled for responsive

--- src/common/Loading/styled.js
+++ src/common/Loading/styled.js
@@ -7,16 +7,26 @@ export const Wrapper = styled.div`
     font-size: 20px;
     line-height: 140%;
     letter-spacing: 0.05em;
-    margin: 82px 0 0 0;
+    margin: 82px 0 140px 0;
     color: ${({ theme }) => theme.elementColors.textImporant};
+
+    @media (max-width:${({ theme }) => theme.breakpoints.large}px){
+        margin: 40px 0 70px 0;
+    }
+    @media (max-width:${({ theme }) => theme.breakpoints.mobile}px){
+        font-size: 16px;
+    }
 `;
 
 export const Info = styled.p`
     margin: 0 0 48px 0;
+    @media (max-width:${({ theme }) => theme.breakpoints.mobile}px){
+        margin: 0 0 32px 0;
+    }
 `;
 
 export const Spinner = styled.img`
-    margin: 0 0 140px 0;
+    margin: 0 ;
     animation: spinner 1s linear infinite;
 
     @keyframes spinner {
@@ -26,5 +36,10 @@ export const Spinner = styled.img`
     100% {
         transform: rotate(360deg);
     }
+    }
+
+    @media (max-width:${({ theme }) => theme.breakpoints.mobile}px){
+        width: 80px;
+        height: 80px;
     }
 `;
