52302c9187afdf17874b019393f61983c71fe486
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-23 20:57:33 +0100

    added new saga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,10 @@
 import { all } from "redux-saga/effects";
+import { githubSaga } from "./common/github/githubSaga";
 import { themeSaga } from "./common/theme/themeSaga";
 
 export default function* rootSaga() {
     yield all([
         themeSaga(),
+        githubSaga(),
     ])
 };
