b4e0fc12de7e1d9f93b9e867d25427c3c854c994
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-23 22:19:20 +0100

    edited Portfolio for fetchGithub

--- src/features/Portfolio/index.js
+++ src/features/Portfolio/index.js
@@ -1,15 +1,37 @@
-import { Tile } from "../../common/Tile";
-import { Description, Header, Icon } from "./styled";
-
+import { useSelector } from "react-redux";
+import { Error } from "./Error";
+import { selectRepos, selectStatus } from "../../common/github/githubSlice";
+import { Tile } from "./Tile";
+import { Description, Header, Icon, TilesWrapper } from "./styled";
+import { Loading } from "./Loading";
+import { nanoid } from "nanoid";
 export const Portfolio = () => {
-
+    const status = useSelector(selectStatus);
+    const repos = useSelector(selectRepos);
     return (
         <>
             <Icon />
             <Header>Portfolio</Header>
             <Description>My recent project</Description>
 
-            <Tile />
+            <TilesWrapper notSuccess={status === "success" ? false : true}>
+                {
+                    status === "loading"
+                        ? <Loading />
+
+                        : status === "success"
+                            ? repos.map((repo) =>
+                                <Tile as="li"
+                                    key={nanoid()}
+                                    url={repo.html_url}
+                                    description={repo.description}
+                                    gitUrl={repo.html_url}
+                                    name={repo.name}
+                                />)
+
+                            : <Error />
+                }
+            </TilesWrapper>
 
         </>
     )
