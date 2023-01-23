de2ba80572f7c1e80c4d72e97683555a836097e6
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-23 22:16:35 +0100

    added useEffect with fetchRepos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, theme } from "./common/theme/theme";
 import { selectIsDarkTheme } from "./common/theme/themeSlice";
 import { GlobalStyle } from "./GlobalStyle";
 import Homepage from "./features/Homepage";
 import { Wrapper } from "./styled";
+import { useEffect } from "react";
+import { fetchRepos } from "./common/github/githubSlice";
 
 
 function App() {
+    const dispatch = useDispatch();
     const isDarkTheme = useSelector(selectIsDarkTheme);
+
+    useEffect(() => {
+        dispatch(fetchRepos());
+    }, [dispatch])
+
     return (
         <ThemeProvider theme={isDarkTheme ? darkTheme : theme}>
             <GlobalStyle />
