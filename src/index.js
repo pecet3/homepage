f0c1dec0be34c23fa86917bf8805105f951f4bd8
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-23 22:17:08 +0100

    removed unnecessary imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
 import App from './App';
-import { darkTheme, theme } from './common/theme/theme';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
 
