8742f8218df1a9a1c87872ddc5bfaf7396514ea0
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-25 21:34:36 +0100

    removed logic

--- src/homepage/Portfolio/Tile/index.js
+++ src/homepage/Portfolio/Tile/index.js
@@ -1,25 +1,21 @@
 import { Description, Header, Container, DetailsWrapper, Link, } from "./styled";
 
-export const Tile = ({ description, url, gitUrl, name, empty }) => {
+export const Tile = ({ description, url, gitUrl, name }) => {
     return (
         <>
-            <Container empty={empty ? true : false}>
-                {!empty &&
-                    <>
-                        <Header>{name && name}</Header>
-                        <Description>
-                            {description && description}
-                        </Description>
-                        <DetailsWrapper>
-                            <Link href={url && `https://pecet3.github.io/${name}`} target="_blank">
-                                Go to the demo
-                            </Link>
-                            <Link href={gitUrl && gitUrl} target="_blank">
-                                See Repository
-                            </Link>
-                        </DetailsWrapper>
-                    </>
-                }
+            <Container >
+                <Header>{name && name}</Header>
+                <Description>
+                    {description && description}
+                </Description>
+                <DetailsWrapper>
+                    <Link href={url && `https://pecet3.github.io/${name}`} target="_blank">
+                        Go to the demo
+                    </Link>
+                    <Link href={gitUrl && gitUrl} target="_blank">
+                        See Repository
+                    </Link>
+                </DetailsWrapper>
             </Container>
         </>
     )
