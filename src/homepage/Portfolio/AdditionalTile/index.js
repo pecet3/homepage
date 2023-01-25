8a948084e18ed559b2f81b1c56feaffa8f122b99
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-25 21:33:44 +0100

    StyledLink as wrapper

--- src/homepage/Portfolio/AdditionalTile/index.js
+++ src/homepage/Portfolio/AdditionalTile/index.js
@@ -1,3 +1,4 @@
+import { github } from "../../../common/personalInfo";
 import {
     StyledContainer,
     StyledHeader,
@@ -10,21 +11,23 @@ import {
 
 export const AdditionalTile = ({ login, name, avatar, location, bio, followers, following }) => {
     return (
-        <StyledContainer>
-            <StyledHeader>
-                <div>
-                    {name && name}
-                    <Login>{login && login}</Login>
-                </div>
-                <Avatar src={avatar && avatar} alt="avatar on github" />
-            </StyledHeader>
-            <StyledDescription>
-                {bio && bio}
-            </StyledDescription>
-            <StyledDetailsWrapper>
-                <StyledLink>📌 {location && location}</StyledLink>
-                <StyledLink>🧙‍♂️ {followers && followers} followers, {following && following} following</StyledLink>
-            </StyledDetailsWrapper>
-        </StyledContainer>
+        <StyledLink href={github} target="_blank">
+            <StyledContainer>
+                <StyledHeader>
+                    <div>
+                        {name && name}
+                        <Login>{login && login}</Login>
+                    </div>
+                    <Avatar src={avatar && avatar} alt="avatar on github" />
+                </StyledHeader>
+                <StyledDescription>
+                    {bio && bio}
+                </StyledDescription>
+                <StyledDetailsWrapper>
+                    <StyledLink as="p">📌 {location && location}</StyledLink>
+                    <StyledLink as="p">🧙‍♂️ {followers && followers} followers, {following && following} following</StyledLink>
+                </StyledDetailsWrapper>
+            </StyledContainer>
+        </StyledLink>
     )
 };
