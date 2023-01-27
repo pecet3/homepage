ce3ca354e9a8189c763f3250d04c71849909f242
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-27 11:53:10 +0100

    link to API as const

--- src/common/github/getUser.js
+++ src/common/github/getUser.js
@@ -1,5 +1,7 @@
+const link = "https://api.github.com/users/pecet3";
+
 export const getUser = async () => {
-    const response = await fetch("https://api.github.com/users/pecet3");
+    const response = await fetch(link);
 
     if (!response.ok) {
         throw new Error(response.text);
